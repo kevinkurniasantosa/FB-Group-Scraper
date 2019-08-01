7b4afd3f88c8346c5130a3c8bf0536adba7e78bd
Kevin Kurnia <kevin.kurnia@id.seagroup.com>
2019-08-01 16:26:42 +0700

    third

--- fb_group_scraping.py
+++ fb_group_scraping.py
@@ -22,10 +22,8 @@ print('import successful')
 output = []
 output_filename = 'FB Group Output.json'
 main_url = 'https://www.facebook.com'
-# email = input('Email: ')
-# password = stdiomask.getpass()
-email = 'patrick_er@hotmail.com'
-password = 'facebookpass'
+email = input('Email: ')
+password = stdiomask.getpass()
 
 ### SET UP DRIVER
 # UA = 'Mozilla/5.0 (Linux; Android 4.1.1; GT-N7100 Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/35.0.1916.138 Mobile Safari/537.36 T7/6.3'
@@ -91,163 +89,164 @@ def main():
         loop = 1
         for prop in list_property:
             print('---------------------- Prop ' + str(loop))
-            # # Title
-            # try:
-            #     title = prop.find('div', class_='_l53').text.strip()
-            # except Exception as err:
-            #     print(str(err))
-            #     title = '-'
-            # print('Title: ' + title)
+            # Title
+            try:
+                title = prop.find('div', class_='_l53').text.strip()
+            except Exception as err:
+                print(str(err))
+                title = '-'
+            print('Title: ' + title)
 
-            # # Price
-            # try:
-            #     price = prop.find('div', class_='_l57').text
-            # except:
-            #     price = '-'
-            # print('Price: ' + price)
+            # Price
+            try:
+                price = prop.find('div', class_='_l57').text
+            except:
+                price = '-'
+            print('Price: ' + price)
             
-            # # Location
-            # try:
-            #     location_name = prop.find('div', class_='_l58').text.strip()
-            # except:
-            #     location_name = '-'
-            # print('Location: ' + location_name)
+            # Location
+            try:
+                location_name = prop.find('div', class_='_l58').text.strip()
+            except:
+                location_name = '-'
+            print('Location: ' + location_name)
 
-            # # Longitude & Latitude
-            # if location_name == '-':
-            #     latitude = '-'
-            #     longitude = '-'
-            # else:
-            #     try:
-            #         location_str = location_name + ', Hong Kong'
-            #         chrome_options2 = webdriver.ChromeOptions()
-            #         # chrome_options2.add_argument("--headless")
-            #         second_driver = webdriver.Chrome(options=chrome_options2)
-            #         second_driver.get('https://www.latlong.net/')
-            #         lat_long_container = second_driver.find_element_by_xpath("//div[@class='col-7 graybox']")
-            #         input_place = lat_long_container.find_element_by_xpath("//input[@id='place']")
-            #         input_place.send_keys(location_str)
-            #         lat_long_container.find_element_by_xpath("//button[@id='btnfind']").click()
-            #         time.sleep(random_int)
-            #         lat_long = second_driver.find_element_by_xpath("//div[@class='row bg-gray border']").find_element_by_id('latlngspan').text
-            #         # print('Lat Long: ' + lat_long)
-            #         x = re.match("\((.+), (.+)\)", str(lat_long))
-            #         latitude = x.group(1)
-            #         longitude = x.group(2)
-            #         second_driver.close()
-            #     except:
-            #         latitude = '-'
-            #         longitude = '-'
-            # print('Latitude: ' + latitude)
-            # print('Longitude: ' + longitude)
+            # Longitude & Latitude
+            if location_name == '-':
+                latitude = '-'
+                longitude = '-'
+            else:
+                try:
+                    location_str = location_name + ', Hong Kong'
+                    chrome_options2 = webdriver.ChromeOptions()
+                    # chrome_options2.add_argument("--headless")
+                    second_driver = webdriver.Chrome(options=chrome_options2)
+                    second_driver.get('https://www.latlong.net/')
+                    lat_long_container = second_driver.find_element_by_xpath("//div[@class='col-7 graybox']")
+                    input_place = lat_long_container.find_element_by_xpath("//input[@id='place']")
+                    input_place.send_keys(location_str)
+                    lat_long_container.find_element_by_xpath("//button[@id='btnfind']").click()
+                    time.sleep(random_int)
+                    lat_long = second_driver.find_element_by_xpath("//div[@class='row bg-gray border']").find_element_by_id('latlngspan').text
+                    # print('Lat Long: ' + lat_long)
+                    x = re.match("\((.+), (.+)\)", str(lat_long))
+                    latitude = x.group(1)
+                    longitude = x.group(2)
+                    second_driver.close()
+                except:
+                    latitude = '-'
+                    longitude = '-'
+            print('Latitude: ' + latitude)
+            print('Longitude: ' + longitude)
 
-            # # Description
-            # try:
-            #     description = prop.find('div', {'data-testid' : 'post_message'}).text.strip()
-            #     description = description.replace('See More', '')
-            # except:
-            #     description = '-'
-            # print('Description: ' + description)
+            # Description
+            try:
+                description = prop.find('div', {'data-testid' : 'post_message'}).text.strip()
+                description = description.replace('See More', '')
+            except:
+                description = '-'
+            print('Description: ' + description)
 
-            # # Original URL
-            # try:
-            #     original_url = prop.find('span', class_='fsm fwn fcg').a['href']
-            #     original_url = main_url + original_url
-            # except:
-            #     original_url = '-'
-            # print('Original URL: ' + original_url)
+            # Original URL
+            try:
+                original_url = prop.find('span', class_='fsm fwn fcg').a['href']
+                original_url = main_url + original_url
+            except:
+                original_url = '-'
+            print('Original URL: ' + original_url)
 
-            # # Date created
-            # try:
-            #     date_created_container = prop.find('div', class_='_5pcp _5lel _2jyu _232_').abbr
-            #     date_created = date_created_container['title']
-            # except:
-            #     date_created = '-'
-            # print('Date created: ' + str(date_created))
+            # Date created
+            try:
+                date_created_container = prop.find('div', class_='_5pcp _5lel _2jyu _232_').abbr
+                date_created = date_created_container['title']
+            except:
+                date_created = '-'
+            print('Date created: ' + str(date_created))
 
-            # # Phone number
-            # phone_number = '-'
-            # try: 
-            #     if 'https://api.whatsapp.com/send?phone=' in description:
-            #         x = re.match(".+https:\/\/api.whatsapp.com\/send\?phone=(\d+)", str(description))
-            #         phone_number = x.group(1)
-            #     elif 'https://api.whatsapp.com/send?' in description:
-            #         x = re.match(".+https:\/\/api.whatsapp.com\/send\?(\d+)", str(description))
-            #         phone_number = x.group(1)
-            #     elif 'https://wa.me/' in description:
-            #         x = re.match(".+https://wa.me/(\d+)", str(description))
-            #         phone_number = x.group(1)
-            #     elif '+852' in description:
-            #         try:
-            #             x = re.match(".+852\s(\d+)", str(description))
-            #             phone_number = '852' + x.group(1)
-            #             try:
-            #                 phone_number = phone_number.replace(' ', '')
-            #             except:
-            #                 pass
-            #         except:
-            #             pass
-            #         try:
-            #             x = re.match(".+852\s(\d+\s\d+)", str(description))
-            #             phone_number = '852' + x.group(1)
-            #             try:
-            #                 phone_number = phone_number.replace(' ', '')
-            #             except:
-            #                 pass
-            #         except:
-            #             pass
-            # except Exception as err:
-            #     phone_number = '-'
-            #     print('Error getting phone number: ' + str(err))
-            # print('Phone number: ' + phone_number)
+            # Phone number
+            phone_number = '-'
+            try: 
+                if 'https://api.whatsapp.com/send?phone=' in description:
+                    x = re.match(".+https:\/\/api.whatsapp.com\/send\?phone=(\d+)", str(description))
+                    phone_number = x.group(1)
+                elif 'https://api.whatsapp.com/send?' in description:
+                    x = re.match(".+https:\/\/api.whatsapp.com\/send\?(\d+)", str(description))
+                    phone_number = x.group(1)
+                elif 'https://wa.me/' in description:
+                    x = re.match(".+https://wa.me/(\d+)", str(description))
+                    phone_number = x.group(1)
+                elif '+852' in description:
+                    try:
+                        x = re.match(".+852\s(\d+)", str(description))
+                        phone_number = '852' + x.group(1)
+                        try:
+                            phone_number = phone_number.replace(' ', '')
+                        except:
+                            pass
+                    except:
+                        pass
+                    try:
+                        x = re.match(".+852\s(\d+\s\d+)", str(description))
+                        phone_number = '852' + x.group(1)
+                        try:
+                            phone_number = phone_number.replace(' ', '')
+                        except:
+                            pass
+                    except:
+                        pass
+            except Exception as err:
+                phone_number = '-'
+                print('Error getting phone number: ' + str(err))
+            print('Phone number: ' + phone_number)
 
-            # Images
-            # try:
-            #     loop = loop + 1
-            #     image_url_arr = []
-            #     if prop.find('div', class_='_2a2q _65sr') != None:
-            #         # driver.find_element_by_xpath("//div[@role='feed']/div[1]/a[@class='_5dec _xcx']").click()
-            #         wait.until(lambda driver: driver.find_element_by_xpath("//div[@role='feed']/div[{}]/div[1]/div[3]/div[1]/div[2]/div[3]/div[1]/div[2]/div/a[@class='_5dec _xcx']".format(loop))).click()
-            #         time.sleep(2) 
-            #         soup_modal = BeautifulSoup(driver.page_source, 'html.parser')
-            #         image_container = soup_modal.find('div', class_='_3ffp')
-            #         list_images = soup_modal.find('div', class_='_3ffs').find_all('li')
-            #         print('Num of images: ' + str(len(list_images)))
+            Images
+            try:
+                loop = loop + 1
+                image_url_arr = []
+                if prop.find('div', class_='_2a2q _65sr') != None:
+                    # driver.find_element_by_xpath("//div[@role='feed']/div[1]/a[@class='_5dec _xcx']").click()
+                    wait.until(lambda driver: driver.find_element_by_xpath("//div[@role='feed']/div[{}]/div[1]/div[3]/div[1]/div[2]/div[3]/div[1]/div[2]/div/a[@class='_5dec _xcx']".format(loop))).click()
+                    time.sleep(2) 
+                    soup_modal = BeautifulSoup(driver.page_source, 'html.parser')
+                    image_container = soup_modal.find('div', class_='_3ffp')
+                    list_images = soup_modal.find('div', class_='_3ffs').find_all('li')
+                    print('Num of images: ' + str(len(list_images)))
 
-            #         image = image_container.img
-            #         image_url = image['src']
-            #         print(image_url)
-            #         image_url_arr.append(image_url)    
+                    image = image_container.img
+                    image_url = image['src']
+                    print(image_url)
+                    image_url_arr.append(image_url)    
 
-            #         for z in range(1, len(list_images)): 
-            #             # pagination = driver.find_elements_by_xpath("//li[@role='presentation']")[z]
-            #             # driver.execute_script("arguments[0].click();", pagination)
-            #             driver.find_element_by_css_selector('body').send_keys(Keys.DOWN)
-            #             time.sleep(1)
-            #             # image = image_container.img
-            #             # image_url = image['src']
-            #             image_url = driver.find_element_by_xpath("//div[@class='_3ffp']").find_element_by_tag_name('img').get_attribute('src')
-            #             print(image_url)
-            #             image_url_arr.append(image_url)
+                    for z in range(1, len(list_images)): 
+                        # pagination = driver.find_elements_by_xpath("//li[@role='presentation']")[z]
+                        # driver.execute_script("arguments[0].click();", pagination)
+                        driver.find_element_by_css_selector('body').send_keys(Keys.DOWN)
+                        time.sleep(1)
+                        # image = image_container.img
+                        # image_url = image['src']
+                        image_url = driver.find_element_by_xpath("//div[@class='_3ffp']").find_element_by_tag_name('img').get_attribute('src')
+                        print(image_url)
+                        image_url_arr.append(image_url)
                         
-            #         # close_button = driver.find_element_by_xpath("//button[@class='_3-9a _50zy _50-1 _50z_ _5upp _42ft']")
-            #         # close_button.click()
-            #         driver.find_element_by_css_selector('body').click()
-            #     else:
-            #         pass    
-            # except Exception as err:
-            #     print('Error getting images: ' + str(err))
-            # print('Image URL: ' + str(image_url_arr))
+                    # close_button = driver.find_element_by_xpath("//button[@class='_3-9a _50zy _50-1 _50z_ _5upp _42ft']")
+                    # close_button.click()
+                    driver.find_element_by_css_selector('body').click()
+                else:
+                    pass    
+            except Exception as err:
+                print('Error getting images: ' + str(err))
+            print('Image URL: ' + str(image_url_arr))
 
             # Images
             try:
                 image_url_arr = []
                 image_container = prop.find('div', class_='_2a2q _65sr')
                 list_image = image_container.find_all('a')
-                print('Num of images: ' + list_image)
+                print('Num of images: ' + str(len(list_image)))
                 
                 for image in list_image:
                     image_url = image['data-ploi']
+                    print(image_url)
                     image_url_arr.append(image_url)
             except Exception as err:
                 print('Error getting images: ' + str(err))
@@ -264,44 +263,43 @@ def main():
             print('User\'s name: ' + user_name)
             print('User\'s profile url: ' + str(user_profile_url))
 
-            # # User avatar URL
-            # try:
-            #     res = requests.get(user_profile_url, headers={'User-Agent': 'Mozilla/5.0'})
-            #     soup_profile = BeautifulSoup(res.text, 'html.parser')
-            #     user_avatar_url = soup_profile.find('img', class_='_11kf img')['src']
-            # except:
-            #     user_avatar_url = '?'
-            # print('User avatar url: ' + str(user_avatar_url))
+            # User avatar URL
+            try:
+                res = requests.get(user_profile_url, headers={'User-Agent': 'Mozilla/5.0'})
+                soup_profile = BeautifulSoup(res.text, 'html.parser')
+                user_avatar_url = soup_profile.find('img', class_='_11kf img')['src']
+            except:
+                user_avatar_url = '?'
+            print('User avatar url: ' + str(user_avatar_url))
              
-            # json_output = {
-            #     "title": title,
-            #     "price": price,
-            #     "location_name": location_name,
-            #     "latitude": latitude,
-            #     "longitude": longitude,
-            #     "description": description,
-            #     "original_url": original_url,
-            #     "date_created": date_created,
-            #     "phone_number": phone_number,
-            #     "images": [
-            #     ],
-            #     "facebook_user": {
-            #         "name": user_name,            
-            #         "avatar_url": user_avatar_url,
-            #         "profile_url": user_profile_url
-            #     }
-            # }
+            json_output = {
+                "title": title,
+                "price": price,
+                "location_name": location_name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "description": description,
+                "original_url": original_url,
+                "date_created": date_created,
+                "phone_number": phone_number,
+                "images": [
+                ],
+                "facebook_user": {
+                    "name": user_name,            
+                    "avatar_url": user_avatar_url,
+                    "profile_url": user_profile_url
+                }
+            }
 
-            # Check for duplicates
-            
-
-        #     output.append(json_output)
+           # Check for duplicates
+        
+            output.append(json_output)
             loop = loop + 1
         
         # ###############################################################
 
-        # with open(output_filename, 'w') as output_file:
-        #     json_arr = json.dump(output, output_file, sort_keys=False, indent=4)
+        with open(output_filename, 'w') as output_file:
+            json_arr = json.dump(output, output_file, sort_keys=False, indent=4)
                                                     
     except Exception as err:
         print('Error in main() -> ' + str(err))
